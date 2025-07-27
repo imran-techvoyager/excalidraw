d410c9286b0fed65369f4203c493987c8f7842ec
imran-techvoyager <imran.techvoyager@gmail.com>
2025-07-28 00:07:47 +0530

    Fixing axios

--- apps/web/lib/axios/axiosInstance.ts
+++ apps/web/lib/axios/axiosInstance.ts
@@ -1,8 +1,15 @@
 import axios from "axios";
 import { cookies } from "next/headers";
 
+let url = process.env.NEXT_PUBLIC_HTTP_URL;
+console.log("url0", url);
+if (url && !url.endsWith("/api/v1")) {
+  url = url + "/api/v1";
+  console.log("url1", url);
+}
+
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_HTTP_URL,
+  baseURL: url,
   withCredentials: true,
 });
 
